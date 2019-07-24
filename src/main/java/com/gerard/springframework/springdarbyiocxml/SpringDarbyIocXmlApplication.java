237fb5abd27c22e57f9bef5dd96223a593c06038
Gerard <Debold.gerard@gmail.com>
2019-07-24 13:50:55 -0400

    Initial commit

---
+++ src/main/java/com/gerard/springframework/springdarbyiocxml/SpringDarbyIocXmlApplication.java
@@ -0,0 +1,13 @@
+package com.gerard.springframework.springdarbyiocxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDarbyIocXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDarbyIocXmlApplication.class, args);
+    }
+
+}

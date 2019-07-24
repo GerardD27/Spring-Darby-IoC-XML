a845d2dc93d160939d9e9a75bb7b41cf967c580b
Gerard <Debold.gerard@gmail.com>
2019-07-24 14:08:39 -0400

    Setup coach interface and implementing classes.Created MyApp class to test the implementation

---
+++ src/main/java/com/gerard/springframework/springdarbyiocxml/com/luv2code/springdemo/Coach.java
@@ -0,0 +1,5 @@
+package com.gerard.springframework.springdarbyiocxml.com.luv2code.springdemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/gerard/springframework/springdarbyiocxml/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,8 @@
+package com.gerard.springframework.springdarbyiocxml.com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+}

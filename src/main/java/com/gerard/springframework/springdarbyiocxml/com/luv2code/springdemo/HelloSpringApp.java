f764ad2c8e69ae5189b352ca4cf068ee88b833fb
Gerard <Debold.gerard@gmail.com>
2019-07-25 08:42:44 -0400

    Added config XML file and HelloSpringApp to allow spring to create the Coach instance for us based on the configuration.

---
+++ src/main/java/com/gerard/springframework/springdarbyiocxml/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,18 @@
+package com.gerard.springframework.springdarbyiocxml.com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+    public static void main(String[] args) {
+        // load the spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve the bean form the spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // close the context
+        context.close();
+    }
+}

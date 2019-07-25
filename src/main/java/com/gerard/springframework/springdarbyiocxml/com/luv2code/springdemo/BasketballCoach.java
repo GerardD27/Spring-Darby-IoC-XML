ba83aad42428f31acf894f17fa81428ec1777e77
Gerard <Debold.gerard@gmail.com>
2019-07-25 08:51:53 -0400

    Practice Activity #2: Created a new Coach implementation and used Spring IoC to get an instance

---
+++ src/main/java/com/gerard/springframework/springdarbyiocxml/com/luv2code/springdemo/BasketballCoach.java
@@ -0,0 +1,9 @@
+package com.gerard.springframework.springdarbyiocxml.com.luv2code.springdemo;
+
+public class BasketballCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Shoot 10 free throws";
+    }
+}
